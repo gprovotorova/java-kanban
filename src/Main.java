f7243dad76418671d393848144f9583517fd7874
Galina Karlova <provotorova.gala@yandex.ru>
2023-03-15 17:43:30 +0300

    fix: исправлена инициализация подзадач при объявлении эпика; test: добавлен повторный вызов методов вывода всех задач, подзадач и эпиков, для проверки методов удаления

--- src/Main.java
+++ src/Main.java
@@ -1,122 +1,132 @@
+import manager.HistoryManager;
+import manager.InMemoryTaskManager;
+import manager.Managers;
 import model.Epic;
-import model.Status;
 import model.Subtask;
 import model.Task;
-import manager.Manager;
+import model.Status;
 
 import java.util.ArrayList;
 
-public class Main {
-
+public class Main { 
     public static void main(String[] args) {
 
-        Manager manager = new Manager();
+        HistoryManager memoryHistoryManager = Managers.getDefaultHistory();
+        InMemoryTaskManager taskManager = new InMemoryTaskManager(memoryHistoryManager);
 
         //Создание задачи 1
-        int taskID = manager.addNewTask(new Task("Покупка подарка", "Купить и упаковать подарок подруге", Status.NEW));
-        System.out.println("Create new task: " + taskID);
-        System.out.println("Information about task: " + manager.getTask(taskID).toString());
-
+        int taskId = taskManager.addNewTask(new Task("Покупка подарка", "Купить и упаковать подарок подруге", Status.NEW));
+        System.out.println("Create new task: " + taskId);
+        System.out.println("Information about task: " + taskManager.getTask(taskId).toString());
+        
         //Получение по идентификатору
-        Task taskFromManager = manager.getTask(taskID);
+        Task taskFromManager = taskManager.getTask(taskId);
         System.out.println("Get task: " + taskFromManager);
-
+        
         //Обновление
         taskFromManager.setName("Подарок подруге");
-        manager.updateTask(taskFromManager);
+        taskManager.updateTask(taskFromManager);
         System.out.println("Update task: " + taskFromManager);
-
-
+        
         //Создание задачи 2
-        taskID = manager.addNewTask(new Task("Купить платье", "Купить черное длинное платье", Status.InProgress));
-        System.out.println("Create new task: " + taskID);
-        System.out.println("Information about task: " + manager.getTask(taskID).toString());
+        taskId = taskManager.addNewTask(new Task("Купить платье", "Купить черное длинное платье", Status.IN_PROGRESS));
+        System.out.println("Create new task: " + taskId);
+        System.out.println("Information about task: " + taskManager.getTask(taskId).toString());
 
-        //Получение по идентификатору
-        taskFromManager = manager.getTask(taskID);
-        System.out.println("Get task: " + taskFromManager);
+        //Просмотр истории
+        System.out.println("Your viewing history:" + taskManager.getHistory().toString());
 
         //Обновление
         taskFromManager.setDescription("Купить короткое черное платье");
-        manager.updateTask(taskFromManager);
+        taskManager.updateTask(taskFromManager);
         System.out.println("Update task: " + taskFromManager);
-
+        
         //Создание эпика 1
         ArrayList<Subtask> listOfSubtasks = new ArrayList<>();
-
-        int epicID = manager.addNewEpic(new Epic("Подготовка вечеринки", "Вечеринка, чтобы отпраздновать " +
-                "день рождения", listOfSubtasks));
-        int subtaskID = manager.addNewSubtask(new Subtask("Выбрать тему", "...", epicID, Status.DONE));
-        listOfSubtasks.add(manager.getSubtask(subtaskID));
-        subtaskID = manager.addNewSubtask(new Subtask("Выбрать место", "...", epicID, Status.DONE));
-        listOfSubtasks.add(manager.getSubtask(subtaskID));
-        subtaskID = manager.addNewSubtask(new Subtask("Купить торт", "...", epicID, Status.DONE));
-        listOfSubtasks.add(manager.getSubtask(subtaskID));
-        manager.getEpic(epicID).setSubtasks(listOfSubtasks);
-        manager.setEpicStatus(epicID);
-        System.out.println("Create new epic: " + epicID);
-        System.out.println("Information about epic: " + manager.getEpic(epicID).toString());
+        int epicId = taskManager.addNewEpic(new Epic("Подготовка вечеринки", "Вечеринка, чтобы отпраздновать " +
+                "день рождения"));
+        taskManager.getEpic(epicId).setSubtasks(listOfSubtasks);
+        System.out.println("Information about epic: " + taskManager.getEpic(epicId).toString());
+        int subtaskId = taskManager.addNewSubtask(new Subtask("Выбрать тему", "...", epicId, Status.DONE));
+        listOfSubtasks.add(taskManager.getSubtask(subtaskId));
+        subtaskId = taskManager.addNewSubtask(new Subtask("Выбрать место", "...", epicId, Status.NEW));
+        listOfSubtasks.add(taskManager.getSubtask(subtaskId));
+        subtaskId = taskManager.addNewSubtask(new Subtask("Купить торт", "...", epicId, Status.IN_PROGRESS));
+        listOfSubtasks.add(taskManager.getSubtask(subtaskId));
+        taskManager.getEpic(epicId).setSubtasks(listOfSubtasks);
+        System.out.println("Create new epic: " + epicId);
+        System.out.println("Information about epic: " + taskManager.getEpic(epicId).toString());
 
         //Обновление подзадачи
-        Epic epicFromManager = manager.getEpic(3);
+        Epic epicFromManager = taskManager.getEpic(3);
         epicFromManager.setDescription("Выбрать тему и цвета вечеринки");
-        manager.updateEpic(manager.getEpic(epicFromManager.getId()));
+        taskManager.updateEpic(taskManager.getEpic(epicFromManager.getId()));
         System.out.println("Update epic: " + epicFromManager);
 
         //Создание эпика 2
-        ArrayList<Subtask> listOfNewSubtasks = new ArrayList<>();
-        epicID = manager.addNewEpic(new Epic("Торт", "Торт для дня рождения", listOfNewSubtasks));
-        subtaskID = manager.addNewSubtask(new Subtask("Выбрать кондитерскую", "...", epicID, Status.InProgress));
-        listOfNewSubtasks.add(manager.getSubtask(subtaskID));
-        manager.getEpic(epicID).setSubtasks(listOfNewSubtasks);
-        manager.setEpicStatus(epicID);
-        System.out.println("Create new epic: " + epicID);
-        System.out.println("Information about epic: " + manager.getEpic(epicID).toString());
+        listOfSubtasks = new ArrayList<>();
+        epicId = taskManager.addNewEpic(new Epic("Торт", "Торт для дня рождения"));
+        taskManager.getEpic(epicId).setSubtasks(listOfSubtasks);
+        subtaskId = taskManager.addNewSubtask(new Subtask("Выбрать кондитерскую", "...", epicId, Status.IN_PROGRESS));
+        listOfSubtasks.add(taskManager.getSubtask(subtaskId));
+        taskManager.getEpic(epicId).setSubtasks(listOfSubtasks);
+        System.out.println("Create new epic: " + epicId);
+        System.out.println("Information about epic: " + taskManager.getEpic(epicId).toString());
 
         //Получение эпика по идентификатору
-        epicFromManager = manager.getEpic(epicID);
+        epicFromManager = taskManager.getEpic(epicId);
         System.out.println("Get epic: " + epicFromManager);
 
         //Получение подзадачи по идентификатору
-        Subtask subtaskFromManager = manager.getSubtask(6);
+        Subtask subtaskFromManager = taskManager.getSubtask(6);
         System.out.println("Get subtask: " + subtaskFromManager);
 
         //Обновление
         epicFromManager.setDescription("Медовик");
-        manager.updateEpic(epicFromManager);
+        taskManager.updateEpic(epicFromManager);
         System.out.println("Update epic: " + epicFromManager);
 
+        //Просмотр истории
+        System.out.println("Your viewing history:" + taskManager.getHistory().toString());
 
         //Получение списка всех задач
-        System.out.println("All tasks: " + manager.getAllTasks());
+        System.out.println("All tasks: " + taskManager.getAllTasks());
 
         //Получение списка всех эпиков
-        System.out.println("All epics: " + manager.getAllEpics());
+        System.out.println("All epics: " + taskManager.getAllEpics());
 
         //Получение списка всех подзадач
-        System.out.println("All subtasks: " + manager.getAllSubtasks());
+        System.out.println("All subtasks: " + taskManager.getAllSubtasks());
 
         //Получение всех задач одного эпика
-        epicFromManager = manager.getEpic(epicID);
-        System.out.println("All subtasks of epic: " + manager.getAllSubtasksOfEpic(epicFromManager));
+        epicFromManager = taskManager.getEpic(epicId);
+        System.out.println("All subtasks of epic: " + taskManager.getAllSubtasksOfEpic(epicFromManager));
 
         //Удаление по идентификатору задачи
-        manager.deleteByIDTasks(1);
-
+        taskManager.deleteByIdTasks(1);
+        
         //Удаление по идентификатору подзадачи
-        manager.deleteByIDSubtask(6);
+        taskManager.deleteByIdSubtask(6);
 
         //Удаление по идентификатору эпика
-        manager.deleteByIDEpic(3);
+        taskManager.deleteByIdEpic(3);
 
         //Удаление всех задач
-        manager.deleteAllTasks();
+        taskManager.deleteAllTasks();
 
         //Удаление всех подзадач
-        manager.deleteAllSubtasks();
+        taskManager.deleteAllSubtasks();
 
         //Удаление всех эпиков
-        manager.deleteAllEpics();
+        taskManager.deleteAllEpics();
 
+        //Получение списка всех задач
+        System.out.println("All tasks: " + taskManager.getAllTasks());
+
+        //Получение списка всех эпиков
+        System.out.println("All epics: " + taskManager.getAllEpics());
+
+        //Получение списка всех подзадач
+        System.out.println("All subtasks: " + taskManager.getAllSubtasks());
     }
 }

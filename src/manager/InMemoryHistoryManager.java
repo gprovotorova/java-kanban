148ed36392aac85d8b879133a30dfd12a3e20f14
Galina Karlova <provotorova.gala@yandex.ru>
2023-04-03 18:18:37 +0300

    feat: добавлен метода удаления задачи - removeTask, refactor: изменен вывод истории просмотра

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -2,11 +2,11 @@ package manager;
 
 import model.Task;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private LinkedList viewHistory = new LinkedList<>();
+
+    private CustomLinkedList<Task> viewHistory = new CustomLinkedList<>();
 
     public InMemoryHistoryManager() {
         this.viewHistory = viewHistory;
@@ -14,16 +14,74 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void addTask(Task task) {
-        if (viewHistory.size() > 9){
-            viewHistory.removeFirst();
-            viewHistory.add(task);
-        } else {
-            viewHistory.add(task);
-        }
+        viewHistory.linkLast(task);
+    }
+
+    @Override
+    public void removeTask(int id){
+        viewHistory.remove(id);
     }
 
     @Override
     public List<Task> getAll() {
-        return viewHistory;
+        return viewHistory.getTasks();
+    }
+
+    public class CustomLinkedList<T> {
+        public Node head = null;
+        public Node tail = null;
+        protected int id = 0;
+
+        public int setId(int id) {
+            return ++id;
+        }
+
+        private LinkedList<Task> viewHistoryForWork = new LinkedList<>();
+        HashMap<Integer, Node> reviewedTask = new HashMap<>();
+
+        public void linkLast(Task task){
+            if (viewHistoryForWork.contains(task)){
+                int index = viewHistoryForWork.indexOf(task);
+                Task taskForWork = viewHistoryForWork.get(index);
+                var node = new Node(taskForWork, tail, head);
+                reviewedTask.put(setId(id), node);
+                removeNode(node);
+                remove(index);
+                viewHistoryForWork.addLast(task);
+            } else {
+                viewHistoryForWork.addLast(task);
+            }
+        }
+
+
+        public ArrayList<Task> getTasks() {
+            ArrayList<Task> addedTasks = new ArrayList<>();
+            for (int i = 0; i < viewHistoryForWork.size(); i++) {
+                addedTasks.add(i, viewHistoryForWork.get(i));
+            }
+            return addedTasks;
+        }
+
+        public void removeNode(Node node) {
+            reviewedTask.remove(node);
+            Node temp = head, prev = null;
+            Task taskForSearch = node.getTask();
+            if (temp != null && temp.task == taskForSearch) {
+                head = temp.next;
+                return;
+            }
+            while (temp != null && temp.task != taskForSearch) {
+                prev = temp;
+                temp = temp.next;
+            }
+            if (temp == null){
+                return;
+            }
+            prev.next = temp.next;
+        }
+
+        public void remove(int id) {
+            viewHistoryForWork.remove(id);
+        }
     }
 }

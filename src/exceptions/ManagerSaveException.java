9dd181bda650f71f83839146d84813f64db9df5f
Galina Karlova <provotorova.gala@yandex.ru>
2023-04-24 23:19:51 +0300

    fix: добавлены конструкторы класса

--- src/exceptions/ManagerSaveException.java
+++ src/exceptions/ManagerSaveException.java
@@ -3,5 +3,20 @@ package exceptions;
 import java.io.IOException;
 
 public class ManagerSaveException extends RuntimeException {
+    public ManagerSaveException() {
+        super();
+    }
+
+    public ManagerSaveException(String message) {
+        super(message);
+    }
+
+    public ManagerSaveException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ManagerSaveException(Throwable cause) {
+        super(cause);
+    }
 
 }

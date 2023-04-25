43c0dcb68ec413c5b17858094a3b5a87dab41471
Galina Karlova <provotorova.gala@yandex.ru>
2023-04-25 21:22:08 +0300

    fix: исправлены замечания

--- src/exceptions/ManagerSaveException.java
+++ src/exceptions/ManagerSaveException.java
@@ -3,10 +3,6 @@ package exceptions;
 import java.io.IOException;
 
 public class ManagerSaveException extends RuntimeException {
-    public ManagerSaveException() {
-        super();
-    }
-
     public ManagerSaveException(String message) {
         super(message);
     }
